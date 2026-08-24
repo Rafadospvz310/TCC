e2b9d4848e6c56db6672d33156adbf3040e3e345
RAFAEL SANTANA DA SILVA <026052@fatecararas.com>
2026-08-24 09:22:32 -0300

    mundanças no html para conectar no django

--- logaritimo/logaritimo/urls.py
+++ logaritimo/logaritimo/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('estoque/', include('estoque.urls')),
+   path('estoque/', include(('estoque.urls', 'estoque'), namespace='estoque')),
     path('compras/', include('compras.urls')),
     path('vendas/', include('vendas.urls')),
     path('', include('usuarios.urls')),
