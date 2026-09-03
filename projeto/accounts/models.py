c463edaf89aa76bf6a07df02e95eac616d52221d
Leonardo354126 <leonardo.pc.cunha@gmail.com>
2026-09-03 20:28:34 -0300

    Adicionando a funcionalidade de diferenciar usuarios

--- projeto/accounts/models.py
+++ projeto/accounts/models.py
@@ -2,5 +2,13 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Usuario(AbstractUser):
-    pass
+    PERMISSOES = ( 
+        ('ADMINISTRADOR', 'Administrador'),
+        ('ESTOQUE', 'Estoque'),
+        ('VENDEDOR', 'Vendedor'),
+        ('CAIXA', 'Caixa'),
+        ('COMPRAS', 'Compras')
+    )
+
+    role = models.CharField(max_length=20, choices=PERMISSOES, default='VENDEDOR')
     
--- projeto/app/views.py
+++ projeto/app/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+
+def acesso_estoque(user):
+    return user.is_authenticated and (user.role == 'ESTOQUE' or user.role == 'ADMINISTRADOR')
+
+def acesso_vendas(user):
+    return user.is_authenticated and (user.role == 'VENDEDOR' or user.role == 'ADMINISTRADOR')
+
+def acesso_caixa(user):
+    return user.is_authenticated and (user.role == 'CAIXA' or user.role == 'ADMINISTRADOR')
+
+def acesso_compras(user):
+    return user.is_authenticated and (user.role == 'COMPRAS' or user.role == 'ADMINISTRADOR')
+
+def login_view(request):
+    return render(request, 'index.html')
+
 
 def login_view(request):
     return render(request, 'index.html')
@@ -18,23 +35,36 @@ def deletar_usuario_view(request):
 def dashboard_view(request):
     return render(request, 'dashboard_admin.html')
 
+@user_passes_test(acesso_estoque)
 def controle_estoque_view(request):
     return render(request, 'estoque_controle.html')
 
+
+@user_passes_test(acesso_estoque)
 def detalhes_item_view(request):
     return render(request, 'estoque_item.html')
 
+
+@user_passes_test(acesso_estoque)
 def etiquetas_view(request):
     return render(request, 'estoque_etiquetas.html')
 
+
+@user_passes_test(acesso_vendas)
 def nova_venda_view(request):
     return render(request, 'vendas_vendedor.html')
 
+
+@user_passes_test(acesso_caixa)
 def caixa_pdv_view(request):
     return render(request, 'vendas_caixa.html')
 
+
+@user_passes_test(acesso_caixa)
 def relatorio_vendas_view(request):
     return render(request, 'caixa_relatorio.html')
 
+
+@user_passes_test(acesso_compras)
 def painel_compras_view(request):
     return render(request, 'compras.html')
